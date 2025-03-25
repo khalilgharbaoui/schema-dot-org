e78a0e2f22161391d923f180adcfa15a3a107496
Exequiel Rozas <exequiel98@gmail.com>
2025-03-24 23:54:58 -0400

    Add 'about' and 'publisher' properties to website

--- lib/schema_dot_org/thing.rb
+++
@@ -1,8 +0,0 @@
-module SchemaDotOrg
-  class Thing < SchemaType
-    validated_attr :name, type: String, presence: true
-    validated_attr :alternate_name, type: String, allow_nil: true
-    validated_attr :image, type: ImageObject, allow_nil: true
-    validated_attr :same_as, type: String, allow_nil: true
-  end
-end

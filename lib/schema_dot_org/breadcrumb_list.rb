7fc0fbfb5ac60ec2d595afde584df538d0f90816
Exequiel Rozas <exequiel98@gmail.com>
2025-03-26 02:23:29 -0400

    Make the 'item_list_element' an array

--- lib/schema_dot_org/breadcrumb_list.rb
+++ lib/schema_dot_org/breadcrumb_list.rb
@@ -1,5 +1,5 @@
 module SchemaDotOrg
   class BreadcrumbList < SchemaType
-    validated_attr :item_list_element, type: SchemaDotOrg::ListItem, presence: true
+    validated_attr :item_list_element, type: Array, presence: true
   end
 end

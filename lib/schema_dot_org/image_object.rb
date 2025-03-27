4061c4479afbccce078ecf815ab888aec11c947f
Exequiel Rozas <exequiel98@gmail.com>
2025-03-26 20:16:02 -0400

    Allow nil 'width' and 'height' for the ImageObject

--- lib/schema_dot_org/image_object.rb
+++ lib/schema_dot_org/image_object.rb
@@ -1,7 +1,7 @@
 module SchemaDotOrg
   class ImageObject < SchemaType
     validated_attr :url, type: String
-    validated_attr :height, type: Integer
-    validated_attr :width, type: Integer
+    validated_attr :height, type: Integer, allow_nil: true
+    validated_attr :width, type: Integer, allow_nil: true
   end
 end

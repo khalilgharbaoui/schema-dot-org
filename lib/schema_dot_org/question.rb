d1024e4e8f003d89e1b8286f94e6a13f5aba40a9
Exequiel Rozas <exequiel98@gmail.com>
2025-03-26 21:21:35 -0400

    Correctly type 'accepted_answer' and 'suggested_answer'

--- lib/schema_dot_org/question.rb
+++ lib/schema_dot_org/question.rb
@@ -2,8 +2,7 @@ module SchemaDotOrg
   class Question < SchemaType
     validated_attr :name, type: String
     validated_attr :text, type: String, allow_nil: true
-    validated_attr :accepted_answer, type: String, allow_nil: true
-    validated_attr :suggested_answer, type: String, allow_nil: true
-    validated_attr :image, type: ImageObject, allow_nil: true
+    validated_attr :accepted_answer, type: SchemaDotOrg::Answer, allow_nil: true
+    validated_attr :suggested_answer, type: SchemaDotOrg::Answer, allow_nil: true
   end
 end
